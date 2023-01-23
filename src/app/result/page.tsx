d917ea9b8ab96ee2fe08a84faf4eec82770ccabb
Kakeshin <kakeshin.kasitaro@gmail.com>
2023-01-23 11:35:54 +0900

    fix

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -6,17 +6,20 @@ interface Props {
 
 const Page = async ({ searchParams: data }: Props) => {
   const result = await axios.post('http://localhost:3000/api/apex-legends/status', data)
-  const global = result.data.global
-  const hoge = JSON.stringify(global, null, 2)
+  const {
+    name,
+    uid,
+    rank: { rankName, rankImg },
+  } = result.data.global
   return (
     <div>
       <h1>Axe確定</h1>
       <div>
-        <pre>{JSON.stringify(global, null, 2)}</pre>
         <div>
-          <h3>Name: {global.name}</h3>
-          <h3>UID: {global.uid}</h3>
-          <h3>RANK: {global.rank.rankScore}</h3>
+          <h3>Name: {name}</h3>
+          <h3>UID: {uid}</h3>
+          <h3>NOW RANK:{rankName}</h3>
+          <img src={rankImg} alt='rank image' />
         </div>
       </div>
     </div>
