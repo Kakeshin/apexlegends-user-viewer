138ef76af9098cdadfb099d2f9780bb014e3a7ab
Kakeshin <kakeshin.kasitaro@gmail.com>
2023-01-23 18:11:23 +0900

    fix

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,19 +1,31 @@
 import axios from 'axios'
-import styles from './page.module.css'
 
-interface Props {
-  searchParams: { platform: string; userName: string }
+interface PageProps {
+  searchParams?: { platform: string; userName: string }
 }
 
-const Page = async ({ searchParams: data }: Props) => {
-  const result = await axios.post('http://localhost:3000/api/apex-legends/status', data)
+const Page = async ({ searchParams: data }: PageProps) => {
+  // TODO: 後ほど修正
+  // const result = await axios.post('http://localhost:3000/api/apex-legends/status', data)
+  const result = {
+    data: {
+      global: {
+        name: 'Test Name',
+        uid: '475893427549',
+        rank: {
+          rankName: 'Test Rank',
+          rankImg: 'Test Rank Img',
+        },
+      },
+    },
+  }
   const {
     name,
     uid,
     rank: { rankName, rankImg },
   } = result.data.global
   return (
-    <main className={styles.main}>
+    <main>
       <div>
         <h1>Apex Legends User Information</h1>
         <div>
@@ -21,7 +33,6 @@ const Page = async ({ searchParams: data }: Props) => {
             <h3>Name: {name}</h3>
             <h3>UID: {uid}</h3>
             <h3>NOW RANK:{rankName}</h3>
-            <img src={rankImg} alt='rank image' />
           </div>
         </div>
       </div>
